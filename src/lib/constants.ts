2278af32bd0fda0fab5e260c01d429040bec0c2a
yLorde <davisouza2204@gmail.com>
2026-09-20 00:20:52 -0300

    update openylab

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const PACKAGE_NAME = "easy-api-consumer";
 export const PACKAGE_VERSION = "1.2.5";
 export const NPM_URL = "https://www.npmjs.com/package/easy-api-consumer";
-export const GITHUB_URL = "https://github.com/open-ylorde/easy-api-consumer";
+export const GITHUB_URL = "https://github.com/OpenyLab/easy-api-consumer";
 export const AUTHOR_URL = "https://portfolio.ylorde.com.br";
